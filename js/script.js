d032c95d1b628dec79c16b3d1318c17f268147a3
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2020-08-19 04:52:10 +0300

    some scroll animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+// Slick slider
+
 $('.twitter__block').slick({
 	infinite: true,
 	slidesToShow: 1,
@@ -8,3 +11,42 @@ $('.twitter__block').slick({
 	dots: true,
 	arrows: false,
 });
+
+
+// Scroll Animations
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;                            // Чем больше коэффициент, тем меньше надо делать скролл для появления анимации
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				let animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.remove('_active');
+			} else {
+				if (animItem.classList.contains('anim-no-hide')) {
+					animItem.classList.add('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	animOnScroll();
+}
